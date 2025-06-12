d10ca7a74953d277b8319ff00fa8e1a0b3793065
Zhiyan Qian <zqian1@unc.edu>
2025-06-12 18:02:09 -0400

    cleaned up

--- my_functions.py
+++ my_functions.py
@@ -1,14 +1,5 @@
 import numpy as np
 from sklearn.decomposition import PCA # PCA reduces high dimensions
-# from sklearn.cross_decomposition import CCA #aligning
-
-# def align_cca(X, Y, n_components = None):
-#     # computes CCA allignment between X and Y
-#     if n_components is None:
-#         n_components = min(X.shape[1], Y.shape[1])
-#     cca = CCA(n_components = n_components)
-#     X_c, Y_c = cca.fit_transform(X, Y)
-#     return X_c, Y_c, cca
 
 def CCA(A, B, align=None):
     """
